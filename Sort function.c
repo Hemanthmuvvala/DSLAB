e16dde25688b13f095a04e4026caab2c07ccea04
Hemanthmuvvala <160225341+Hemanthmuvvala@users.noreply.github.com>
2024-02-16 15:01:48 +0530

    Update Sort function.c

--- Sort function.c
+++ Sort function.c
@@ -1,1 +1,28 @@
+#include<stdio.h>
+void swap(int*a,int*b){
+int temp=*a;
+*a=*b;
+*b=temp;
+}
+void sort(int ar[],int n){
+int i,j;
 
+
+for(i=0;i<n;i++)
+for(j=i+1;j<n;j++)
+{
+if(ar[i]<ar[j+1])
+swap(&ar[i],&ar[j+1]);
+}
+for(i=0;i<n;i++)
+printf("%d",ar[i]);
+}
+int main(){
+int i,ar[100],n;
+printf("enter the number of elements ");
+scanf("%d",&n);
+printf("enter the elements of array");
+for(i=0;i<n;i++)
+scanf("%d",&ar[i]);
+sort(ar,n);
+}
