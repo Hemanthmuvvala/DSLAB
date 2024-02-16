9d0a61439eea531e18a069e3b8cfa1a3c61889f7
Hemanthmuvvala <160225341+Hemanthmuvvala@users.noreply.github.com>
2024-02-16 15:20:13 +0530

    Create rev array.c

---
+++ rev array.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void reverse( int arr[],int n)
+{
+int i;
+
+printf(" The reversed array");
+for(int i=n;i>=0;i--)
+{
+printf("%d\t",arr[i]);
+}
+
+}
+int main()
+{
+int arr[100],n,i;
+printf("enter the size of the array");
+scanf("%d",&n);
+printf("enter the array");
+for(int i=0;i<n;i++)
+{
+scanf("%d",&arr[i]);
+}
+reverse(arr,n);
+
+
+
+}
