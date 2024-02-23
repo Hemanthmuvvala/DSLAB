f25654cb931f9ee9abd87e991c1829c62a0d6dcc
hemanth venkata naga pavan kumar <160225341+Hemanthmuvvala@users.noreply.github.com>
2024-02-23 14:28:29 +0530

    Create insertion sort.c

---
+++ insertion sort.c
@@ -0,0 +1,55 @@
+
+#include<stdio.h>
+void readarr(int ar[],int n) {
+    int i;
+    for(i=0; i<n; i++)
+        scanf("%d",&ar[i]);
+}
+void printarr(int ar[],int n)
+{
+    for(int i=0; i<n; i++)
+        printf("%d\n",ar[i]);
+}
+void swap(int *a,int *b) {
+    int temp=*a;
+    *a=*b;
+    *b=temp;
+}
+void sort(int a[],int n)
+{
+    int i,j,key;
+    for(i=1; i<n; i++) 
+    {
+  
+        key=a[i];
+        j=i-1;
+        while(j>=0 && a[j]>key)
+        {
+            a[j+1]=a[j];
+            j=j-1;
+               
+        }
+        a[j+1]=key;
+        
+        
+    }
+}
+int main()
+{
+    int ar[100],n;
+    printf("enter the no of elements");
+    scanf("%d",&n);
+    printf("enter the elements");
+    readarr(ar,n);
+    sort(ar,n);
+    printf("the array after sorting:");
+    printarr(ar,n);
+
+
+
+
+
+
+
+}
+    
